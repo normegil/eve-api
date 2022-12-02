f838d0f9b30d2df94aadbc8c1889857157042216
Olivier Ory <mail@normegil.be>
2022-12-02 16:16:35 +0100

    Request key

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -35,6 +35,9 @@ impl ApplicationAuthFlow {
         let (_, challenge_to_send) = generate_challenge();
         let code = self.request_code(&challenge_to_send)?;
         let tokens = request_tokens(&code, &self.client_id, &challenge_to_send)?;
+        let key = request_key();
+
+        
 
         Ok(())
     }
@@ -119,10 +122,47 @@ fn request_tokens(code: &str, client_id: &str, challenge: &str) -> io::Result<Au
     }
 }
 
+fn request_key() -> io::Result<AuthTokenKey> {
+    let cli = reqwest::blocking::Client::new();
+    let resp = cli.get("https://login.eveonline.com/oauth/jwks").send();
+    let resp = match resp {
+        Err(e) => return Err(io::Error::new(io::ErrorKind::Other, format!("Error when trying to get auth token key for validation: {}", e))),
+        Ok(r) => r,
+    };
+    let body = match resp.text() {
+        Ok(b) => b,
+        Err(e) => return Err(io::Error::new(io::ErrorKind::Other, format!("Error when trying to read response body for auth token key for validation: {}", e))), 
+    };
+
+    let key = match serde_json::from_str::<AuthTokenKeyResponseBody>(&body) {
+        Err(e) => return Err(io::Error::new(io::ErrorKind::Other, format!("Error when trying to deserialize response body for auth token from json: {}", e))),
+        Ok(b) => {
+            let k = b.keys.iter().find(|b| b.kid == "JWT-Signature-Key");
+            match k {
+                None => return Err(io::Error::new(io::ErrorKind::Other, format!("No key found: {:?}", b))),
+                Some(k) => k.clone(),
+            }
+        }, 
+    };
+    Ok(key)
+}
+
 #[derive(Deserialize)]
 struct AuthTokenResponseBody {
     token_type: String,
     expires_in: i32,
     access_token: String,
     refresh_token: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct AuthTokenKeyResponseBody {
+    keys: Vec<AuthTokenKey>
+}
+
+#[derive(Deserialize, Debug, Clone)]
+struct AuthTokenKey {
+    alg: String,
+    kid: String,
+    n: String
 }
